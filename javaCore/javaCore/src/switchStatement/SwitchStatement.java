35b2baee77cfa69901030612f7cae58e8c18e94d
shivamkonkar <shivamkonkar25@gmail.com>
2021-04-23 09:59:54 +0530

    new projects

---
+++ javaCore/javaCore/src/switchStatement/SwitchStatement.java
@@ -0,0 +1,44 @@
+package switchStatement;
+
+import java.util.Scanner;
+
+public class SwitchStatement {
+
+	public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("enter number betweem 1 and 5");
+	
+	int n = sc.nextInt();
+	
+	switch(n) {
+	
+	case 1 :
+		System.out.println("one");
+		break;
+		
+	case 2 :
+		System.out.println("two");
+		break;
+		
+	case 3 :
+		System.out.println("three");
+		break;
+		
+	case 4 :
+		System.out.println("four");
+		break;
+		
+	case 5 :
+		System.out.println("five");
+		break;
+		
+	default :
+		System.out.println("No Match");
+	
+	}
+
+	}
+
+}
